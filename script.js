aacdf0fd8e7d0e35ad270e06ed8c138adc23e884
Treeconnect37 <tdelahaye148@gmail.com>
2025-08-01 11:10:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function showSection(id) {
   document.querySelectorAll('section').forEach(sec => sec.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 }
+  function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("active");
+  }
